f23ce21bc3693a68673b5c723e0f1aea71759c6b
Qiu Chaofan <qcf@ecnelises.com>
2023-11-12 23:53:59 +0800

    Add basic linux definition

---
+++ include/intro.h
@@ -0,0 +1,8 @@
+#ifndef REVLIBC_INTRO_H_
+#define REVLIBC_INTRO_H_
+
+#if defined(linux)
+#include "linux_like/all.h"
+#endif
+
+#endif // REVLIBC_INTRO_H_
---
+++ include/linux_like/all.h
@@ -0,0 +1,14 @@
+#ifndef REVLIBC_LINUX_ALL_H_
+#define REVLIBC_LINUX_ALL_H_
+
+#include <stdint.h>
+
+typedef uint16_t sa_family_t;
+typedef unsigned speed_t;
+typedef unsigned tcflag_t;
+typedef int clockid_t;
+typedef void* timer_t;
+typedef int key_t;
+typedef unsigned id_t;
+
+#endif // REVLIBC_LINUX_ALL_H_
